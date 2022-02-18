456295a8aae098b9be20fbb6fdc09e012df510bc
Hirwa-Pacifique <hirwaa69@gmail.com>
2022-02-19 00:19:48 +0800

    Ask for a date!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import tkinter as tk
 from tkinter import messagebox
 messagebox.askyesno("Question","Will you be my girlfriend?")
+messagebox.askyesno("Congratulations", "Do you wanna go on a date?")
