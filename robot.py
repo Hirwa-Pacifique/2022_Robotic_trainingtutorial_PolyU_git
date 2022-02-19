e3dd48024421c4b279e09b8498396e200e879df1
Hirwa-Pacifique <hirwaa69@gmail.com>
2022-02-19 16:52:19 +0800

    test

---
+++ robot.py
@@ -0,0 +1,8 @@
+print("hello User!")
+x = int(input("enter a number: "))
+if x%2 == 0:
+    print("x is even number")
+    print("the number entered is", x)
+else:
+    print("x is odd number")
+    print("the number entered is", x)
